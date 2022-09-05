9177a35986549dc3dcd5ee6ea4ffe374d3c1a751
Talita Fiacadori <97335932+tvfiacadori@users.noreply.github.com>
2022-09-05 13:14:58 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+  mario.classList.add('jump');
+
+
+  setTimeout(( ) => {
+  
+  mario.classList.remove('jump');
+  }, 500);
+             
+ }
+
+ const loop = setInterval(() => {
+
+  const pipePosition = pipe.offsetLeft;
+  //console.log(pipePosition);
+
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px' , '');
+
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 90) {
+    
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = './img/game-over.png';
+    mario.style.width = '75px';
+    mario.style.marginLeft = '50px';
+
+    clearInterval(loop);
+
+    //console.log(loop);
+  }
+
+ }, 10);
+
+document.addEventListener('keydown', jump);
